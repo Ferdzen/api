0a86f1354e2da9bed84c4a7626232540fb653ed3
Fernanda Inacio <fernandaivp63@gmail.com>
2024-07-08 17:25:51 -0300

    criacao de métodos para crypto senha

--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require github.com/badoux/checkmail v1.2.4 // indirect
+require (
+	github.com/badoux/checkmail v1.2.4 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+)
--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,6 +1,7 @@
 package modelos
 
 import (
+	"api/seguranca"
 	"errors"
 	"strings"
 	"time"
@@ -24,7 +25,9 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return err
 	}
 
-	usuario.formatar()
+	if err := usuario.formatar(etapa); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -50,8 +53,19 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, err := seguranca.Hash(usuario.Senha)
+		if err != nil {
+			return err
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+
+	return nil
 }
